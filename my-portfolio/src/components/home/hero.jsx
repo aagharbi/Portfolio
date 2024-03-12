6909d77c6fd96effa3f5de046ba3db840d7243e9
aagharbi <ayman.agharbi01@gmail.com>
2024-03-11 20:05:57 -0500

    Changes were changed

--- my-portfolio/src/components/home/hero.jsx
+++ my-portfolio/src/components/home/hero.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+'use client'
+import React, {useEffect, useState} from "react";
+import useSystemTheme from "../themeRef";
 
 const Hero = () => {
+    const theme = useSystemTheme();
+
     return (
         <div className="flex flex-col
         h-full w-full 
-        justify-center text-center">
-            <div className="flex flex-col md:flex-row justify-center">
-                <div className="p-8 md:text-left">
+        justify-center">
+            <div className="flex flex-col lg:flex-row justify-center w-full">
+                <div className="p-8 text-center lg:text-left">
                     <h1 className="text-6xl font-extrabold">
                         Front-End Developer
                     </h1>
                     <p className="text-lg font-light">
                         Hi, my name is Ayman Agharbi.  Feel free to look around!
                     </p>
+
                 </div>
-
-                    <img className="h-64 blob-animate" src="https://placehold.co/600x400" alt="placeholder" />
-
-                
+                <div className="p-8 self-center">
+                    <div className={`h-min-48 w-min-48 h-64 w-64 blob-animate ${theme === 'dark' ? `bg-gray-200` : `bg-gray-800`}`}>
+                    <img className="h-min-48 w-min-48 h-64 w-64 blob-animate" src="/ayman.jpeg" alt="placeholder" />
+                    </div>                
+                </div>                
             </div>
             
 
